55d717b8fb432807a905e094329d7b5be4ab73d4
tallan groberg <tallan.taven@gmail.com>
2019-06-05 15:30:55 -0600

    user choice working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OptionBox from "./components/OptionBox";
+import rock from "./images/rock.jpg";
+import paper from "./images/paper.jpg";
+import scissor from "./images/maxresdefault.jpg";
+import { Container, Header, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { userChoice: null, compChoice: null, };
+
+// i want to choose from a an array a random string and place that value in compChoice value
+
+choices = ['rock', 'paper', 'scissors']
+
+  Compick = (choices) => {
+    const {compChoice} = [Math.floor(Math.random() * choices.length)]({compChoice: choices})
+  }
+
+  optionClick = (option) => {
+      const {userChoice} = this.state;
+      this.setState({ userChoice: option, });
+    };
+
+  render() {
+    return(
+      <Container style={{ marginTop: "25px", }}>
+        <Header as="h1">React RPS</Header>
+        <hr />
+        <OptionBox name="rock" img={rock} optionClick={this.optionClick} />
+        <OptionBox name="paper" img={paper} optionClick={this.optionClick} />
+        <OptionBox name="scissors" img={scissor} optionClick={this.optionClick} />
+      </Container>
+    );
+  };
+};
 
 export default App;
---
+++ src/Compick.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+
+
+  Compick = (opp) => {
+    [Math.floor(Math.random() * ['rock', 'paper', 'scissors'].length)];
+    const {compChoice} = this.state;
+    this.setstate({ compChoice: opp, })
+  }
