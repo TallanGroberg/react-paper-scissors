ece526c33aaafe7477e976d393c0e980a3d7c064
tallan groberg <tallan.taven@gmail.com>
2019-06-06 09:33:21 -0600

    winner logic complete

--- src/App.js
+++ src/App.js
@@ -10,7 +10,42 @@ class App extends React.Component {
 
 // i want to display the image of the userChoice
 
+  winner = () => {
+    switch (true) {
 
+      case this.state.userChoice === 'rock'&& this.state.compChoice === 'rock':
+      console.log('its a tie')
+      break;
+      case this.state.userChoice === 'rock'&& this.state.compChoice === 'scissors':
+      console.log('player wins')
+      break;
+      case this.state.userChoice === 'rock'&& this.state.compChoice === 'paper':
+      console.log('computer wins')
+      break;
+
+
+      case this.state.userChoice === 'paper'&& this.state.compChoice === 'paper':
+      console.log("tie with paper")
+      break;
+      case this.state.userChoice === 'paper' && this.state.compChoice === 'scissors':
+      console.log('player lost')
+      break;
+      case this.state.userChoice === 'paper'&& this.state.compChoice === 'rock':
+      console.log('player win')
+      break;
+
+      case this.state.userChoice === 'scissors'&& this.state.compChoice === 'scissors':
+      console.log("tie with scissors")
+      break;
+      case this.state.userChoice === 'scissors'&& this.state.compChoice === 'rock':
+      console.log('player lost')
+      break;
+      case this.state.userChoice === 'scissors'&& this.state.compChoice === 'paper':
+      console.log('player win')
+      break;
+
+    }
+  }
 
   compick = () => {
     const choices = ['rock', 'paper', 'scissors'];
@@ -20,10 +55,11 @@ class App extends React.Component {
   optionClick = (option) => {
 
       const {userChoice} = this.state;
-      this.setState({ userChoice: option, compChoice: this.compick() });
+      this.setState({ userChoice: option, compChoice: this.compick()}, () => this.winner())
     };
 
   render() {
+
     return(
       <Container style={{ marginTop: "25px", }}>
         <Header as="h1">React RPS</Header>
@@ -34,6 +70,7 @@ class App extends React.Component {
         <div>you chose: {this.state.userChoice}
         <hr />
         computer chose: {this.state.compChoice}</div>
+      <div>{this.state.winner}</div>
       </Container>
     );
   };
